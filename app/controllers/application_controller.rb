b37af98fbaf11ce22a222677a3a31c78516ffc31
EC2 Default User <ec2-user@ip-172-31-34-164.us-east-2.compute.internal>
2018-08-03 17:35:59 +0000

    first trial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,38 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    return erb :home
+  end
+  
+  get '/index' do
     return erb :index
   end
+  
+  post '/index' do
+    @career_response = params[:career]
+    @lowest_ratio_response = params[:ratio]
+    @wage_gap_response = params[:wage_gap]
+    @pay_parity_response = params[:wage_gap_close]
+  
+  career_quiz = Career_quiz.new
+  @result = career_quiz.play_quiz(@career_response)
+  # @result1 = career_quiz.play_quiz(@lowest_ratio_response)
+  
+  if @result == "teaching"
+    erb :teaching
+  elsif  @result == "engineering"
+    erb :engineering
+  elsif @result == "law"
+    erb :law
+  elsif @result == "politics"
+    erb :politics
+  elsif @result == "computer_engineering"
+    erb :computer_engineering
+  elsif @result == "medical"
+    erb :medical
+  elsif @result == "acting_film"
+    erb :acting_film
+  end
+  
+  end 
 end
--- app/models/sample_model.rb
+++ app/models/sample_model.rb
@@ -1,3 +1,46 @@
-class SampleModel
-  attr_accessor :name, :age
+class Career_quiz
+    def initialize 
+        @statistics = ["teaching/education", "acting/film", "engineering", "law", "medical", "computer science", "politics", 20, 41]
+    end
+  
+    def q1(answer)
+        if answer == @statistics[0]
+            "teaching"
+        elsif answer == @statistics[1]
+            "acting_film"
+        elsif answer == @statistics[2]
+            "engineering"
+        elsif answer == @statistics[3]
+            "law"
+        elsif answer == @statistics[4]
+            "medical"
+        elsif answer == @statistics[5]
+            "computer_engineering"
+        elsif answer == @statistics[6]
+            "politics"
+        end
+    end
+  
+# def q2(answer)
+#   if @answer == @statistics[5]
+#   end 
+# end
+
+# def q3(answer)
+#   if @answer == @statistics[7]
+#   end 
+# end 
+
+# def q4(answer)
+#   if @answer == @statistics[8]
+#   end 
+# end 
+
+def play_quiz(career)
+    q1(career)
+    # q2(lowest_ratio)
+    # q3(wage_gap)
+    # q4(pay_parity)
+end 
+
 end
